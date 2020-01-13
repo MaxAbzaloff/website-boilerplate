0588dc228cbd12142c0120dcaf79bb5e729de50c
Max Abzalov <max.abzalov@ylab.io>
2020-01-13 18:28:49 +1000

    :mount_fuji: added work with images to gulp file and deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,37 @@
-const {
-  src,
-  dest,
-  watch,
-  series,
-  parallel,
-  task,
-} = require('gulp');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const cleanCSS = require('gulp-clean-css');
-const del = require('del');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
-const inject = require('gulp-inject');
+const { src, dest, watch, series, parallel, task } = require("gulp");
+const concat = require("gulp-concat");
+const cleanCSS = require("gulp-clean-css");
+const del = require("del");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
+const inject = require("gulp-inject");
+const image = require("gulp-image");
+const changed = require("gulp-changed");
 
 const PATHES = {
   styles: {
-    src: 'src/styles/**/*.scss',
-    dest: 'public'
+    src: "src/styles/**/*.scss",
+    dest: "public"
   },
   html: {
-    src: 'src/index.html',
-    dest: 'public',
+    src: "src/index.html",
+    dest: "public"
   },
+  images: {
+    src: "src/assets/*.+(png|jpg|gif)",
+    dest: "public/assets"
+  }
 };
 
 function clean() {
-  return del([ PATHES.styles.dest ]);
+  return del([PATHES.styles.dest]);
 }
 
 function styles() {
   return src(PATHES.styles.src)
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass().on("error", sass.logError))
     .pipe(cleanCSS())
-    .pipe(concat('main.min.css'))
+    .pipe(concat("main.min.css"))
     .pipe(dest(PATHES.styles.dest));
 }
 
@@ -44,15 +41,21 @@ function watchStyles() {
 
 function html() {
   return src(PATHES.html.src)
-  .pipe(dest(PATHES.html.dest))
-  .pipe(src(PATHES.html.dest))
-  .pipe(
-    inject(
-      src([ PATHES.styles.dest + '/**/*.css' ], { read: false }),
-      { relative: true },
+    .pipe(dest(PATHES.html.dest))
+    .pipe(src(PATHES.html.dest))
+    .pipe(
+      inject(src([PATHES.styles.dest + "/**/*.css"], { read: false }), {
+        relative: true
+      })
     )
-  )
-  .pipe(dest(PATHES.html.dest))
+    .pipe(dest(PATHES.html.dest));
+}
+
+function images() {
+  return src(PATHES.images.src)
+    .pipe(changed(PATHES.images.dest))
+    .pipe(image())
+    .pipe(dest(PATHES.images.dest));
 }
 
 function watchHtml() {
@@ -60,21 +63,17 @@ function watchHtml() {
 }
 
 function serve() {
-  browserSync
-    .init({
-      server: 'public'
-    });
-  
-  browserSync.watch('public/**/*.*').on('change', browserSync.reload);
+  browserSync.init({
+    server: "public"
+  });
+
+  browserSync.watch("public/**/*.*").on("change", browserSync.reload);
 }
 
-const build = series(clean, styles, html);
-const dev = series(
-  build,
-  parallel(serve, watchStyles, watchHtml),
-);
+const build = series(clean, styles, html, images);
+const dev = series(build, parallel(serve, watchStyles, watchHtml));
 
-task('build', build);
-task('serve', dev);
+task("build", build);
+task("serve", dev);
 
-task('default', dev);
+task("default", dev);
